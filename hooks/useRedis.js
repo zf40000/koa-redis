0772ff01736ca8ae0b8cea944bbdea28c1fd8461
jeff zeng <jeff@eqinfo.com.cn>
2022-09-14 20:40:08 +0800

    fix: 沈浩加入真实的场景

--- hooks/useRedis.js
+++ hooks/useRedis.js
@@ -1,105 +1,185 @@
-import { createClient } from 'redis';
-import { v4 as uuidv4 } from 'uuid';
+import { createClient } from 'redis'
+import { v4 as uuidv4 } from 'uuid'
+import HTTP from 'http'
+import md5 from 'md5-node'
+
+const key = '36c75e3f460371bc86b193a7d94877b6'
+
+const POST_OPTIONS = {
+  host: '127.0.0.1',
+  port: 80,
+  path: '/lis_server/apiLis.php',
+  method: 'POST',
+  headers: '',
+}
 
 // 阻塞等待超时时间,默认600
-const blockTimeout = 20;
+const blockTimeout = 20
 // 消息队列key
-const msgKey = 'msgKey';
+const msgKey = 'msgKey'
 // 备份队列key
-const msgKeyBak = 'msgKeyBak';
+const msgKeyBak = 'msgKeyBak'
 
 // 模拟消费
-const doConsum = (client, task) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(async () => {
-      //模拟消费失败,期望:备份队列的消息保留,
-      // return reject('消费失败'+task);
+// const doConsum = (client, task) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(async () => {
+//       //模拟消费失败,期望:备份队列的消息保留,
+//       // return reject('消费失败'+task);
 
-      // 消费成功,真实的消费是向php发送api请求.
-      console.log("消费成功:", task);
-      await client.lRem(msgKeyBak, 1, task);
-      console.log('从备份队列中删除消息,删除后备份队列:', await client.lRange(msgKeyBak, 0, -1))
-      return resolve('ok')
-    }, 10);
-  });
+//       // 消费成功,真实的消费是向php发送api请求.
+//       console.log('消费成功:', task)
+//       await client.lRem(msgKeyBak, 1, task)
+//       console.log(
+//         '从备份队列中删除消息,删除后备份队列:',
+//         await client.lRange(msgKeyBak, 0, -1)
+//       )
+//       return resolve('ok')
+//     }, 10)
+//   })
+// }
+
+const doConsum = (client, task) => {
+  const tmp = Date.parse(new Date()).toString().substring(0, 10)
+  const POST_DATA = {
+    method: 'lis.async.updateTask',
+    timestamp: tmp,
+    version: 'lis',
+  }
+
+  POST_DATA['params'] = task
+  POST_DATA['token'] = createToken(POST_DATA)
+  const bodyString = JSON.stringify(POST_DATA)
+  console.log(bodyString)
+  POST_OPTIONS['headers'] = {
+    'Content-Type': 'application/json',
+    'Content-Length': bodyString.length,
+  }
+  return new Promise((resolve, reject) => {
+    // 创建 http 连接
+    // const REQUEST = HTTP.request(POST_OPTIONS, requestOnResponse)
+    const REQUEST = HTTP.request(POST_OPTIONS, function (serverFeedback) {
+      if (serverFeedback.statusCode == 200) {
+        var body = ''
+        serverFeedback
+          .on('data', function (data) {
+            body += data
+          })
+          .on('end', async () => {
+            console.log('http body', body)
+            var resultObject = JSON.parse(body)
+            if (resultObject.rsp == 'succ') {
+              await client.lRem(msgKeyBak, 1, task)
+              console.log(
+                '从备份队列中删除消息,删除后备份队列:',
+                await client.lRange(msgKeyBak, 0, -1)
+              )
+              return resolve('ok')
+            }
+          })
+      } else {
+        console.log('end data 500 ')
+      }
+    })
+    // 添加事件监听
+    REQUEST.on('error', requestOnError)
+    // REQUEST.on('timeout', requestOnTimeout)
+    // 设置超时
+    REQUEST.setTimeout(6000)
+    console.log('http write')
+    // 通过连接发送数据
+    REQUEST.write(bodyString, 'utf8')
+    REQUEST.end()
+  })
 }
 
 // 模拟重复消费:上次消费失败后,重启消费服务时需要重试,
 const retryConsum = async (client) => {
-  const listTask = await client.lRange(msgKeyBak, 0, -1);
+  const listTask = await client.lRange(msgKeyBak, 0, -1)
   if (listTask.length > 0) {
-    console.log('重试上次失败的消费任务', listTask);
+    console.log('重试上次失败的消费任务', listTask)
     for (const task of listTask) {
-      const result = await doConsum(client, task).catch(e => {
-        console.log('重试消费失败', e);
-      });
+      const result = await doConsum(client, task).catch((e) => {
+        console.log('重试消费失败', e)
+      })
       if (result !== 'ok') {
-        return false;
+        return false
       }
     }
-
   }
-  return true;
+  return true
 }
 
 // 接受php的请求
 
 // 启动消费服务
 const startConsum = async (client) => {
-  const key = msgKey;
-  const key1 = msgKeyBak;
+  const key = msgKey
+  const key1 = msgKeyBak
 
   // 重试上次失败的消费
-  const retryResult = await retryConsum(client);
+  const retryResult = await retryConsum(client)
   if (!retryResult) {
     // console.log('中断等待/或者扔到失败队列中,继续消费');
-    console.log('重试消费终止');
-    client.quit();
-    return;
+    console.log('重试消费终止')
+    client.quit()
+    return
   }
 
   //阻塞等待时间,超时后重新等待,不要设置为0,可能导致socket连接失效的问题,一般可以设置600s超时
-  console.log(`开始阻塞等待任务,${blockTimeout}s后重连,${msgKey} 队列长度:`, await client.lLen(key), `,${msgKeyBak} 队列长度:`, await client.lLen(msgKeyBak));
+  console.log(
+    `开始阻塞等待任务,${blockTimeout}s后重连,${msgKey} 队列长度:`,
+    await client.lLen(key),
+    `,${msgKeyBak} 队列长度:`,
+    await client.lLen(msgKeyBak)
+  )
 
   // 注意设置timeout,不能无限等待,可能导致socket连接失效的问题
-  const res = await client.brPopLPush(key, key1, blockTimeout);
+  const res = await client.brPopLPush(key, key1, blockTimeout)
   if (res === null) {
     //重新阻塞等待
-    console.log('消费等待超时重新启动消费');
-    await startConsum(client);
-    return;
+    console.log('消费等待超时重新启动消费')
+    await startConsum(client)
+    return
   }
-  console.log('发现待消费任务:', res, '原队列拉取后长度:', await client.lLen(key), '备份队列插入后:', await client.lRange(key1, 0, -1));
-  const _res = await doConsum(client, res).catch(e => {
-    console.log('error fired', e);
-    return;
-  });
+  console.log(
+    '发现待消费任务:',
+    res,
+    '原队列拉取后长度:',
+    await client.lLen(key),
+    '备份队列插入后:',
+    await client.lRange(key1, 0, -1)
+  )
+  const _res = await doConsum(client, res).catch((e) => {
+    console.log('error fired', e)
+    return
+  })
   if (_res !== 'ok') {
-    console.log('中断等待/或者扔到失败队列中,继续消费');
-    client.quit();
-    return;
+    console.log('中断等待/或者扔到失败队列中,继续消费')
+    client.quit()
+    return
   }
   //重新阻塞等待
-  await startConsum(client);
+  await startConsum(client)
 }
 
 // 生产消息
 // data : php 传过来的数据
 const produceMsg = async (client, data) => {
   //为每个元素生成uid
-  const _data = data.map(item => {
-    item.uid = uuidv4();
-    return JSON.stringify(item);
-  });
-  const res = await client.lPush(msgKey, _data);
-  console.log(res, '生产消息成功');
+  const _data = data.map((item) => {
+    item.uid = uuidv4()
+    return JSON.stringify(item)
+  })
+  const res = await client.lPush(msgKey, _data)
+  console.log(res, '生产消息成功')
   // client.quit();
-  return res;
+  return res
 }
 
-let _client = null;
+let _client = null
 
-const createAClient = async()=>{
+const createAClient = async () => {
   const client = createClient({
     // 4.0以后的新格式: https://github.com/redis/node-redis/blob/HEAD/docs/client-configuration.md#reconnect-strategy
     socket: {
@@ -108,57 +188,93 @@ const createAClient = async()=>{
       connectTimeout: 3000,
       // 以下只会在redis down掉时触发,正常timeout导致的重连不会触发
       reconnectStrategy: function (options) {
-        console.log('reconnectStrategy fired', options);
+        console.log('reconnectStrategy fired', options)
         //分别为重试次数,错误,
-        const { attempt, total_retry_time, error, times_connected } = options;
+        const { attempt, total_retry_time, error, times_connected } = options
         if (error && error.code === 'ECONNREFUSED') {
-          return new Error('The server refused the connection');
+          return new Error('The server refused the connection')
         }
-        return undefined;
-      }
+        return undefined
+      },
     },
-  }).on('connect', () => {
-    console.log('redis connected');
-  }).on("ready", () => {
-    // console.log("redis ready");
-  }).on("reconnecting", () => {
-    // timeout后会触发重连,这里需要断开连接,释放内尺寸
-    console.log("redis reconnecting");
-    // 如果quit和end同时发生可能产生错误,这里使用了延迟,避免同时触发,但最好能判断待处理命令是否为空,如果空再进行退出比较安全,to do 判断idle事件
-    // setTimeout(() => {
-    //   client.quit();
-    // }, 2000);
-  }).on('end', () => {
-    //当已和Redis服务器建立的连接被关闭时，client将触发end事件。
-    console.log('*************redis end*******************');
-  }).on('error', async (err) => {
-    console.log('Redis Error', err);
-  }).on('idle', async (err) => {
-    console.log('Redis idle', err);
-  });
-  await client.connect();
-  return client;
+  })
+    .on('connect', () => {
+      console.log('redis connected')
+    })
+    .on('ready', () => {
+      // console.log("redis ready");
+    })
+    .on('reconnecting', () => {
+      // timeout后会触发重连,这里需要断开连接,释放内尺寸
+      console.log('redis reconnecting')
+      // 如果quit和end同时发生可能产生错误,这里使用了延迟,避免同时触发,但最好能判断待处理命令是否为空,如果空再进行退出比较安全,to do 判断idle事件
+      // setTimeout(() => {
+      //   client.quit();
+      // }, 2000);
+    })
+    .on('end', () => {
+      //当已和Redis服务器建立的连接被关闭时，client将触发end事件。
+      console.log('*************redis end*******************')
+    })
+    .on('error', async (err) => {
+      console.log('Redis Error', err)
+    })
+    .on('idle', async (err) => {
+      console.log('Redis idle', err)
+    })
+  await client.connect()
+  return client
 }
 
+// 接受返回的数据
+function requestOnResponse(incomingMessage) {
+  let data = []
+  incomingMessage.on('data', (chunk) => {
+    data.push(...chunk)
+  })
+
+  incomingMessage.on('end', () => {
+    let _date = JSON.parse(new TextDecoder().decode(new Uint8Array(data)))
+    console.log('end data: ', data)
+  })
+}
+
+// 超时 事件处理器
+function requestOnTimeout() {
+  REQUEST.destroy()
+}
+
+// 错误  事件处理器
+function requestOnError(err) {
+  console.log('err: ', err)
+}
+
+function createToken(param) {
+  let stringSignTemp =
+    'timestamp=' + param['tmp'] + '&method=' + param['method'] + '&key=' + key
+
+  let token = md5(stringSignTemp).toString()
+  return token
+}
 export default async function (newClient = false) {
-  let client;
+  let client
   if (newClient === true) {
     // 需要新建连接
-    client= await createAClient();
+    client = await createAClient()
   } else {
     // 使用已建立的client
     if (_client === null) {
-      client = await createAClient();   
-      _client = client;   
+      client = await createAClient()
+      _client = client
     } else {
-      client = _client;
+      client = _client
     }
     // _client = client;
   }
   // 创建一个新得redis连接
   //配置参数的说明参考: https://tuzhu008.github.io/gitbook-Node_cn/Library/node_redis/
-  
-  return { client, startConsum, produceMsg };
+
+  return { client, startConsum, produceMsg }
 }
 
 /*
--- routes/routeRedis.js
+++ routes/routeRedis.js
@@ -1,8 +1,7 @@
-
-import koaRouter from 'koa-router';
+import koaRouter from 'koa-router'
 import useTest from '../hooks/useTest.js'
 import useRedis from '../hooks/useRedis.js'
-const router = koaRouter();
+const router = koaRouter()
 
 // 控制面板\系统和安全\Windows Defender 防火墙
 
@@ -12,128 +11,137 @@ const router = koaRouter();
 //     next()
 // })
 router.get('/', function (ctx, next) {
-    // console.log('这里第二次匹配到路由 /');
-    ctx.body = `
+  // console.log('这里第二次匹配到路由 /');
+  ctx.body = `
     <pre>
         1.测试get参数 <a href='/get?a=aa&b=bb'>访问带get参数的页面</a>
         2,<a href='/test-redis'>测试redis</a>
     </pre>
-    `;
+    `
 })
 
 // 测试get
 router.get('/get', (ctx, next) => {
-    let url = ctx.url;
-    //从request中获取GET请求
-    let request = ctx.request;
-    let req_query = request.query;
-    let req_querystring = request.querystring;
+  let url = ctx.url
+  //从request中获取GET请求
+  let request = ctx.request
+  let req_query = request.query
+  let req_querystring = request.querystring
 
-    //从上下文中直接获取
-    let ctx_query = ctx.query;
-    let ctx_querystring = ctx.querystring;
+  //从上下文中直接获取
+  let ctx_query = ctx.query
+  let ctx_querystring = ctx.querystring
 
-    ctx.body = {
-        url,
-        req_query,
-        req_querystring,
-        ctx_query,
-        ctx_querystring
-    }
-    // console.log('router /get')
-});
+  ctx.body = {
+    url,
+    req_query,
+    req_querystring,
+    ctx_query,
+    ctx_querystring,
+  }
+  // console.log('router /get')
+})
 
 // 测试post
 router.post('/post', (ctx, next) => {
-    // 获得post参数
-    const post = ctx.request.body;
-    console.log('post数据:',post);
+  // 获得post参数
+  const post = ctx.request.body
+  console.log('post数据:', post)
 
-    ctx.body={
-        "success":true,
-        'params':post,
-        "message":'增加数据成功'
-    };
-    //有了这句会执行后面的中间件
-    next();
-});
+  ctx.body = {
+    success: true,
+    params: post,
+    message: '增加数据成功',
+  }
+  //有了这句会执行后面的中间件
+  next()
+})
 
-router.get('/test-redis',async (ctx, next) => {
-    const {client,test} =  await useTest();
-    // console.log(res);
-    const arr = await test(client);
-    ctx.body= arr.map(item=>{
-        return `<p>${item}</p>`;
-    }).join('');
-});
+router.get('/test-redis', async (ctx, next) => {
+  const { client, test } = await useTest()
+  // console.log(res);
+  const arr = await test(client)
+  ctx.body = arr
+    .map((item) => {
+      return `<p>${item}</p>`
+    })
+    .join('')
+})
 
-router.get('/test-block',async (ctx, next) => {
-    const {client,testBlock} =  await useTest();
-    // console.log(res);
-    const arr = await testBlock(client);
-    ctx.body= arr;
-});
-router.get('/test-block-wait',async (ctx, next) => {
-    const {client,testBlockWait} =  await useTest();
-    // console.log(res);
-    testBlockWait(client);
-    ctx.response.type = 'html';
-    ctx.body= `阻塞等待成功, <a href="./test-block-wait?type=produce" target="_blank">点击生产消息</a>`;
-});
+router.get('/test-block', async (ctx, next) => {
+  const { client, testBlock } = await useTest()
+  // console.log(res);
+  const arr = await testBlock(client)
+  ctx.body = arr
+})
+router.get('/test-block-wait', async (ctx, next) => {
+  const { client, testBlockWait } = await useTest()
+  // console.log(res);
+  testBlockWait(client)
+  ctx.response.type = 'html'
+  ctx.body = `阻塞等待成功, <a href="./test-block-wait?type=produce" target="_blank">点击生产消息</a>`
+})
 
-router.get('/testConsum',async (ctx, next) => {
-    const {client,testConsum} =  await useTest();
-    // console.log(res);
-    testConsum(client);
-    ctx.response.type = 'html';
-    ctx.body= `消费服务启动(开始阻塞等待新消息), <a href="./test-block-produce" target="_blank">点击生产消息</a>`;
-});
+router.get('/testConsum', async (ctx, next) => {
+  const { client, testConsum } = await useTest()
+  // console.log(res);
+  testConsum(client)
+  ctx.response.type = 'html'
+  ctx.body = `消费服务启动(开始阻塞等待新消息), <a href="./test-block-produce" target="_blank">点击生产消息</a>`
+})
 // 模拟生产
-router.get('/test-block-produce',async (ctx, next) => {
-    const {client,testBlockProduce} =  await useTest();
-    // console.log(res);
-    await testBlockProduce(client);
-    ctx.body="生产消息完成,观察控制台";
+router.get('/test-block-produce', async (ctx, next) => {
+  const { client, testBlockProduce } = await useTest()
+  // console.log(res);
+  await testBlockProduce(client)
+  ctx.body = '生产消息完成,观察控制台'
 })
 
 // lis请求响应
-router.get('/saveLisData',async (ctx, next) => {
-    const {client,produceMsg} =  await useRedis();
-    const data = [];
+router.all('/saveLisData', async (ctx, next) => {
+  const post = ctx.request.body
+  if (!post) {
+    return
+  }
+  console.log('post', post)
+  const { client, produceMsg } = await useRedis()
+  const data = []
+  data.push(post)
+  console.log('data', data)
 
-    // 创建数据接口样本
-    const demoItem = {
-        InstrID:12,
-        TestID:98,
-        BarcodeID:'',
-        TestTime:'2018-04-24T08:40:00',
-        ItemID:'WBC',
-        Result:'5.99',
-        Result2:'',
-        Result3:'',
-        Result4:'',
-        Flag:0,
-        Remark:'',
-        CreateTime:'2022-06-15T15:02:43.103723+08:00',
-    };
-    for(let i=0;i<1;i++) {
-        data.push(demoItem)
-    }
-    // console.log('开始生产');
-    
-    const res = await produceMsg(client,data);
-    // console.log('produceMsg over',res);
-
-    // client.quit();console.log('quit');const res='quit';
-    ctx.body=res;
-    
+  // 创建数据接口样本
+  // const demoItem = {
+  //   InstrID: 12,
+  //   TestID: 98,
+  //   BarcodeID: '',
+  //   TestTime: '2018-04-24T08:40:00',
+  //   ItemID: 'WBC',
+  //   Result: '5.99',
+  //   Result2: '',
+  //   Result3: '',
+  //   Result4: '',
+  //   Flag: 0,
+  //   Remark: '',
+  //   CreateTime: '2022-06-15T15:02:43.103723+08:00',
+  // }
+  // const demo = {
+  //     method:'lis.analyse.update',
+  //     timestamp:'1655276563',
+  //     token:'c980417d589501bbff555f2cb2f04ers43',
+  //     Params:[demoItem,demoItem,demoItem],
+  // };
+  // for (let i = 0; i < 5; i++) {
+  //   data.push(demoItem)
+  // }
+  await produceMsg(client, data)
+  ctx.body = '响应成功,'
 })
 
-router.get('/info',async (ctx, next) => {
-    const {client,produceMsg} =  await useRedis();
-    const info = await client.info();
-    // client.quit();
-    ctx.body = info;
+router.get('/info', async (ctx, next) => {
+  const { client, produceMsg } = await useRedis()
+  const info = await client.info()
+  // client.quit();
+  ctx.body = info
 })
 
 // // 正式的监听
@@ -146,4 +154,4 @@ router.get('/info',async (ctx, next) => {
 // });
 
 // module.exports = router;
-export default router;
+export default router
